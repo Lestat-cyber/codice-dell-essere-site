cf06923ca0e06bb43727b980d10d6eb38b8f944f
Lestat-cyber <gerasia23@gmail.com>
2025-10-15 07:10:49 +0200

    Create app.tsx

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-br from-emerald-950 via-emerald-900 to-black text-white flex items-center justify-center">
+      <h1 className="text-4xl font-extrabold text-lime-300">Codice dell’Essere</h1>
+    </div>
+  );
+}
